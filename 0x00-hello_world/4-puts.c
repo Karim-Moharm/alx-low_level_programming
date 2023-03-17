66fbd94c0f018f384e358192490a5805e24e4c47
Karim-Moharm <karimmoharm222@gmail.com>
2023-03-16 17:30:28 -0700

    first c prog

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main- main function
+* 
+* return: Always 0 if success
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+	
+}
+
