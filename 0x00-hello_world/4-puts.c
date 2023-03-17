d2704e92e7bc4e5b8f10b3e16c6b5ed8012dec35
Karim-Moharm <karimmoharm222@gmail.com>
2023-03-16 17:46:10 -0700

    first c prog using damn betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+
+/**
+* main- main function
+* Description: printing line
+*
+* Return: Always 0 if success
+*/
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
-	
 }
 
