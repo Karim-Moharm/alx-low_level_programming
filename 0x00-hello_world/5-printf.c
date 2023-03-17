8d10e4155e86e78a067fefb81bb00ab46dc65b5b
Karim-Moharm <karimmoharm222@gmail.com>
2023-03-16 18:01:18 -0700

    printing line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main- main func
+* Description: used to print line
+*
+* Return: return 0 if sucess
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
