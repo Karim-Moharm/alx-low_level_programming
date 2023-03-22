cac18f8f69a5f2f568adc3e8e325c21c1c6a3042
Karim-Moharm <karimmoharm222@gmail.com>
2023-03-21 18:21:15 -0700

    isower

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int check;
+
+	check = _islower('s');
+	_putchar(check + '0');
+	check = _islower('Y');
+	_putchar(check + '0');
+	return (0);
+}
