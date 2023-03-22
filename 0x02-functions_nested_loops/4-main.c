3b0ec5967198186e777b37064fef0ff9c8c3d2ef
Karim-Moharm <karimmoharm222@gmail.com>
2023-03-21 18:41:57 -0700

    is aplha

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int r;
+	r=_isalpha('f');
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
