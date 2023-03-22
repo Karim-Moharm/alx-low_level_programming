9b9d3e59dd4ec2e78aa6867aa2d5508b08478f60
Karim-Moharm <karimmoharm222@gmail.com>
2023-03-22 16:15:40 -0700

    9 time rable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,15 +11,38 @@ void times_table(void)
 	int R; /* row */
 	int C; /* coulmn*/
 	int P; /* product */
+	int n1;
+	int n2;
 
 	for (R = 0; R <= 9; R++)
 	{
 		for (C = 0; C <= 9; C++)
 		{
 			P = R * C;
-			_putchar(P + '0');
-			_putchar(',');
+			if ((P / 10) == 0)
+			{
+				_putchar(P + '0');
 
+				if (C != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				n1 = P / 10;
+				n2 = P % 10;
+				_putchar(n1 + '0');
+				_putchar(n2 + '0');
+
+				if (C != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
 		}
 		_putchar('\n');
 	}
