18f9bfe5588c04b3900e22c48baf8845eaf9f22b
Karim-Moharm <karimmoharm222@gmail.com>
2023-03-21 20:32:49 -0700

    with min and hour

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,26 @@
-#include "main"
+#include "main.h"
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- * Return: Nothing
+ * Return: void
  */
 
 void jack_bauer(void)
 {
+	int Hour;
+	int Min;
+
+	for (Hour = 0; Hour < 24; Hour++)
+	{
+		for (Min = 0; Min < 60; Min++)
+		{
+			_putchar((Hour / 10) + '0');
+			_putchar((Hour % 10) + '0');
+			_putchar(':');
+			_putchar((Min / 10) + '0');
+			_putchar((Min % 10) + '0');
+			_putchar('\n');
+		}
+	}
 
 }
