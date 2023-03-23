8a1255a36f43848d9ca3cafaa85da751d608659b
Karim-Moharm <karimmoharm222@gmail.com>
2023-03-22 19:16:49 -0700

    fib2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+long unsigned int fibonacci(void);
+
+int main(void)
+{
+
+	return (0);
+}
+
+/**
+  * fibonacci -  finds and prints the sum of the even-valued terms in fib series
+  *
+  * Return: sum
+  */
+
+long unsigned int fibonacci(void)
+{
+	int n;
+	int f1 = 1, f2 = 0;
+	int f;
+
+	for (n = 1; n <= 10; n++)
+	{
+		f = f1 + f2;
+
+	}
+}
