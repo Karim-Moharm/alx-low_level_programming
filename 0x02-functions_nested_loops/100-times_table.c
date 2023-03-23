bbbf2ea0a1618ea3b0ffb1c5194229f2b46d4793
Karim-Moharm <karimmoharm222@gmail.com>
2023-03-22 19:28:02 -0700

    time_table2

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_times_table - prints the n times table, starting with 0.
+  * @n: input
+  * Return :void
+  */
+
+void print_times_table(int n)
+{
+
+}
