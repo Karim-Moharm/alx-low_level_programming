0e9467eb69b26c6ba3321101ed1902e1e545793d
Karim-Moharm <karimmoharm222@gmail.com>
2023-03-27 19:24:21 -0700

    print half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,6 +8,21 @@
 
 void puts_half(char *str)
 {
+	int i, c = 0, n;
+
+	while (str[c] != '\0')
+		c++;
+
+	if (c % 2 == 0)
+		n = c / 2;
+
+	else
+		n = (c - 1) / 2;
+
+	for (i = n; i < c; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
 
 
 }
