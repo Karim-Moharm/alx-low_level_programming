a2b268892e5ad2fa13ab5307252b373629b2bc32
Karim-Moharm <karimmoharm222@gmail.com>
2023-03-30 13:26:56 -0700

    initial commit

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -8,8 +8,11 @@
 int main(void)
 {
     char *str;
+    char *str2;
 
     str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    str2 = "My school";
     print_rev(str);
+    print_rev(str2);
     return (0);
 }
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,17 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
 
 /**
-  *rev_string - reverse a string
-  * @s: string
-  * Return: void
+  * rev_string
   */
 
 void rev_string(char *s)
 {
-	int i;
+	int i, c = 0;
 
-	for (i = strlen(s) - 1; i >= 0; i++)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+	while (s[c] != '\0')
+		c++;
+	c -= 1;
+
+
+
 }
