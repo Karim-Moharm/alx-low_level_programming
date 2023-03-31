dae2af7cf2f1919594c8d2a44a243d467546e5fc
Karim-Moharm <karimmoharm222@gmail.com>
2023-03-30 17:54:16 -0700

    all capital

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * string_toupper - function that changes all lowercase letters of a string 
+  * to uppercase.
+  * @str: string
+  * Return: string
+  */
+
+char *string_toupper(char *str)
+{
+	char *tmp =str;
+
+	while (*str != '\0')
+	{
+		if (*str >= 'a' && *str <= 'z')
+			*str -= 32;
+		str++;
+	}
+	return (tmp);
+}
