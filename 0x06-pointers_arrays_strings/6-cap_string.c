f32b9986b91378d917067d551b69b724e63b1482
Karim-Moharm <karimmoharm222@gmail.com>
2023-03-30 18:23:46 -0700

    capitalized all

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,5 +3,10 @@
 /**
   * cap_string - function that capitalizes all words of a string.
   * @str: string
-  * @Return: capitalized string
+  * Return: capitalized string
   */
+
+char *cap_string(char *str)
+{
+
+}
