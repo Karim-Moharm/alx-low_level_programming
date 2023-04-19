bcc579b0b840536dbb320ffcc5a0d9aee314d1be
Karim-Moharm <karimmoharm222@gmail.com>
2023-04-18 17:40:37 -0700

    creating dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - descripe a dog
+  * @name: name of the dog
+  * @age: the age of the dog
+  * @owner: the owner
+  *
+  * Description: Longer description
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
