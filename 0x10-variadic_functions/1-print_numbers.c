2f7e3088a40dd4153b14e6c5f0e9328b3d5081ea
Karim-Moharm <karimmoharm222@gmail.com>
2023-04-20 18:56:46 -0700

    0. Beauty is variable, ugliness is constant

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+  * print_numbers - prints numbers
+  * @separator: separate between numbers
+  * n: amout of numbers
+  * Return: void
+  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, unsigned int));
+		if (i < (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+
+}
+
