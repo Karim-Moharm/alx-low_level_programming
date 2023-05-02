bf08d99841f93dd08be261a1d30e2047619696ce
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-01 18:06:19 -0700

    free the entire list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *curr = *head;
+	listint_t *temp = NULL;
 
 	while (curr != NULL)
 	{
+		temp = curr;
 		curr = curr->next;
-		free(*head);
-		*head = curr;
+		free(temp);
 	}
+	*head = NULL;
+
 }
