478018a95eb6f6a0fb1d3b861e29c056b124fba8
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-01 17:41:39 -0700

    free the list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  * free_listint - free the entire list
+  * @head: head pointer
+  * Return: void
+  */
+
+  void free_listint(listint_t *head)
+  {
+	  listint_t *curr = head;
+
+	  while (curr != NULL)
+	  {
+		  curr = curr->next;
+		  free(head);
+		  head = curr;
+	  }
+  }
