60ccf60b001f664851da2a41a475eb6edb18f947
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-01 18:11:56 -0700

    fixing the error

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,15 @@
 ; the text section
-section .text
+section .text:
 	global _start
 
 _start:
-	mov edx, len
+	mov edx, msg_len
 	mov ecx, msg
 	mov ebx, 1	;stdout
 	mov eax, 4	;system call (sys_wriet)
 
 	mov eax, 1	;system call (sys_exit)
 
-section .data		; this section for declaring data and constant
-	msg db "Hello, Holberton", 0x0a
-	len equ $ -msg
+section .data:		; this section for declaring data and constant
+	msg: db "Hello, Holberton", 0xA
+	msg_len equ $-msg
