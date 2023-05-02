c5e7738d1a156cb885a4622b1075bb770caf7af0
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-02 11:50:41 -0700

    free the first node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+  * pop_listint - delete the head node
+  * @head: pointer to pointer
+  * return: the deleted node's data
+  */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+	int data = 0;
+
+	if (*head == NULL)
+		return (0);
+
+	data = (*head)->n;
+	*head = (*head)->next;
+	/* or *head = temp->next */
+	free (temp);
+	temp = NULL;
+
+	return (data);
+}
