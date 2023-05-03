7472e4f7cd6001d8fdf80ca0053398ce8e3e31ef
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-02 18:49:18 -0700

    fix some issue

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -42,6 +42,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	/* while is false once curr is pointing to node before index of new node */
 	newNode->next = curr->next;
 	curr->next = newNode;
-	
 	return (newNode);
 }
