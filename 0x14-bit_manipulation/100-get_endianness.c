53b0a81b8e2339d4f7831e5ad557833f27356e91
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-04 15:47:01 -0700

    big endian and little

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * get_endianness eck if little endian or big endian
+  * Description: if last bit (MSB) is 0 it will be big endian 
+  * else will be little endian
+  * Return: 0 if big endian, 1 if little endian
+  */
+
+int get_endianness(void)
+{
+	int i = 1;
+	char *c = (char *)&i;
+	/* if c points to 0 it will be big endian, else it will be little */
+
+	if (*c)
+		return (1);
+	return (0);
+}
