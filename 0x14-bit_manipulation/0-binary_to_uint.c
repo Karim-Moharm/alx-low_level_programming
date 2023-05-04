223013386adf5ddd0d51145919c8eedd09bfeb07
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-04 13:19:51 -0700

    convert binary to hex

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * binary_to_uint - convert binary to decimal
+  * @b: binary number
+  * Return: the decimal number or 0 if fails
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int len = 0;
+	int i;
+	unsigned int result = 0;
+	int decimal = 1;
+
+	while (b[len])
+	{
+		len++;
+	}
+	if (b == NULL)
+		return (0);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		if (b[i] == '1')
+			result += decimal;
+		decimal *= 2;
+	}
+	return (result);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,9 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-int _putchar(char c);
+#include <stdio.h>
+#include <stdlib.h>
 
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
 #endif
