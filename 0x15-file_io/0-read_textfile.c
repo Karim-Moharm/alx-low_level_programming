5941e6a25cc2405f1b33778728fb5146e898ea1f
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-08 16:11:46 -0700

    try

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * read_textfile - reads a text and prints it to POSIX
+  * @filename: name of the file
+  * @letters: number of letters it should read and print
+  * Return:  the actual number of letters it could read and print
+  * if file cannot be opend return 0
+  * if filename is NULL return 
+  * if write fails or does not write the expected amount of bytes, return 0
+  */
+  
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd; /* file descriptor */
+	char *buffer = NULL;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd != 0)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	count = read(fd, buffer, letters);
+
+	return (count);
+}
+
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int _putchar(char c);
+
+#endif
