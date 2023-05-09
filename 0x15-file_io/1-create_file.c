80fda01e0b7b3f326771a60dd4cd49382554e446
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-08 17:42:30 -0700

    fixing betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 
 	if (fd < 0)
@@ -26,13 +26,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (write_ret < 0)
 		return (-1);
-	close (fd);
-
+	close(fd);
 	return (1);
 }
 
 /**
-  * _strlen: string length
+  * _strlen - string length
   * @s: string
   * Return: length of string
   */
