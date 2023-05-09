7936847a0cf5b64d76e4e044d8a1d2ef90988929
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-08 19:35:22 -0700

    check

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,21 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
 		exit(97);
 	}
-	fd_from = open("file_from", O_RDONLY);
-	fd_to = open("file_to", O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	fd_from = open(av[1], O_RDONLY);
+	fd_to = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
 
 }
