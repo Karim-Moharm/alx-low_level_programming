2ee481ed763050c9fd2ae6382a743670d6b83a2b
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-08 19:37:20 -0700

    check

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,30 +10,30 @@
   * Return: Always 0
   */
 
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
 	int fd_from, fd_to;
 
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	fd_from = open(av[1], O_RDONLY);
-	fd_to = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	fd_from = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
-
+	return (0);
 }
