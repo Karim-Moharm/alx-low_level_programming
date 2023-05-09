b4f93a21ee99ea6548d59dfd98d84fad01012bef
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-08 17:39:47 -0700

    creating new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd < 0)
 		return (-1);
+	if (text_content == NULL)
+		return (1);
 
 	write_ret = write(fd, text_content, _strlen(text_content));
 
