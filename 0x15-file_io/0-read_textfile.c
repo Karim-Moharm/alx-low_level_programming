83f54363caf5013be026abddd4360081657571bc
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-08 18:20:29 -0700

    update first task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,24 +20,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-
-	if (buffer == NULL)
-		return (0);
-
 	count = read(fd, buffer, letters);
-	if (count != 0)
+	if (count < 0)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	write_ret = write(STDOUT_FILENO, buffer, count);
-	if (write_ret != 0)
+	if (write_ret < 0)
 	{
 		free(buffer);
 		return (0);
