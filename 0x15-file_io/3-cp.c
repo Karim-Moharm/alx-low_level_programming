59aed6d835c049bb6d22985a172aee9928b2aea3
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-08 19:20:58 -0700

    check

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,3 +1,24 @@
 #include "main.h"
 
 /**
+  * main - program that copies the content of a file to another file
+  * @ac: argument count
+  * @av: argument vector
+  * Return: Always 0
+  */
+
+int main(int ac, char **av)
+{
+	int fd_from, fd_to;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		exit(97);
+	}
+	fd_from = open("file_from", O_RDONLY);
+	fd_to = open("file_to", O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+
+
+}
