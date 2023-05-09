24355807e21373c2a81615b377e466cfec72eaeb
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-08 19:50:48 -0700

    copying

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,14 +5,22 @@
 
 /**
   * main - program that copies the content of a file to another file
-  * @ac: argument count
-  * @av: argument vector
+  * @argc: argument count
+  * @argv: argument vector
   * Return: Always 0
   */
 
 int main(int argc, char **argv)
 {
 	int fd_from, fd_to;
+	int close_from_ret;
+	int close_to_ret;
+	int count;
+
+	char *buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+		return (0);
 
 	if (argc != 3)
 	{
@@ -33,6 +41,17 @@ int main(int argc, char **argv)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
+	}
+
+	count = read(fd_from, buffer, 1024);
+
+	close_from_ret = close(fd_from);
+	close_to_ret = close(fd_to);
+
+	if (close_from_ret == -1 || close_to_ret == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE");
+		exit(100);
 	}
 
 	return (0);
