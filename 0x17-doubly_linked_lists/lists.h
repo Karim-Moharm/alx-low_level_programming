772d71a8a7a378a73abd7c7ceae6dd6463730175
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-15 09:50:58 -0700

    modefying header file structure

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,9 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -15,9 +11,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 
