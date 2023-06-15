fbb69060b9ddc9da4cab70c4e6fd8b0f444879b2
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-15 16:38:37 -0700

    finishing get at index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -21,9 +21,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	if (index >= list_len)
 		return (NULL);
-	
 	temp = head;
-	while(index != 0)
+	while (index != 0)
 	{
 		temp = temp->next;
 		index--;
