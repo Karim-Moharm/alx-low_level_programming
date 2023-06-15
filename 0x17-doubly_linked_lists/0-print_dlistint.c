c8b57be37759cdbd5b592f2a5a356ce60d43bb17
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-15 10:06:12 -0700

    printing with returnig num of nodes

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: head pointer
+ * Return: number of Nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t size = 0;
+	const dlistint_t *curr = NULL;
+	curr = h;
+
+	while(curr != NULL)
+	{
+		size++;
+		printf("%d\n", curr -> n);
+		curr = curr -> next;
+	}
+	return (size);
+}
