55b6b72bf4d0a00621a169a7005bea96e4eca1be
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-15 20:17:59 -0700

    initila commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -43,7 +43,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	/* in case of insertion at begin */
 	if (idx == 0)
 	{
-		newNode = add_dnodeint(*&h, n);
+		newNode = add_dnodeint(h, n);
 		return (newNode);
 	}
 
@@ -57,7 +57,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	/* in case of insertion at end */
 	if (!temp->next)
 	{
-		newNode = add_dnodeint_end(*&h, n);
+		newNode = add_dnodeint_end(h, n);
 		return (newNode);
 	}
 
