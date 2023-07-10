f7ab234339030ce29f1558357ab865a576b2c2aa
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-09 19:17:51 -0700

    test betty

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,11 @@
+#include "hash_table.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to newly created hash table, NULL if somethig wrong
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+}
