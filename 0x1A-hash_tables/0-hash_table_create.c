3f281c94c2d027f84467c23064fcb53c69609a53
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-09 20:53:35 -0700

    fix creating a table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,14 +12,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
 	unsigned int i = 0;
-	
+
 	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
 	{
 		return (NULL);
 	}
 	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_node_t*) * size);
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!hash_table->array)
 	{
