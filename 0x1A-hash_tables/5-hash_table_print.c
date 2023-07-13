37ccd6df899e49826593f3f15b5f79c1191079af
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-12 19:31:02 -0700

    free the table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	unsigned long int i = 0;
+
+	if (!ht)
+		printf("{}");
+	else
+	{
+		while (i < ht->size)
+		{
+			i++;
+		}
+	}
+}
+
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int long i = 0;
+	hash_node_t *ht_node = NULL;
+	hash_node_t *ht_temp = NULL;
+	
+	if (!ht)
+		return;
+
+	while (i < ht->size)
+	{
+		if (ht->array[i])
+		{
+			ht_node = ht->array[i];
+			while (ht_node != NULL)
+			{
+				ht_temp = ht_node->next;
+				free(ht_node->key);
+				free(ht_node->value);
+				free(ht_node);
+				ht_node = ht_temp;
+			}
+		}
+		i++;
+	}
+	free(ht->array);
+}
