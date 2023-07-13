5d4cd11c13405e638017270db2fa2d09bd2598d2
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-12 18:06:07 -0700

    test add element

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_table.h"
+
+/**
+ * hash_table_set - adds a element to hash table
+ *
+ * Description: adds an element to hash table and in case of
+ * collision add the new node at the beginning of the list
+ *
+ * @ht: the hash table
+ * @key: the key of hash element
+ * @value: the value of that key
+ *
+ * Return: 1 if successful, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+	hash_node_t *ht_curr_node = NULL;
+
+	index = key_index(key, ht->size)
+	if (key == NULL || ht == NULL || strlen(key) == 0 || index < 0)
+		return (0);
+	ht_curr_node = ht->array[index];
+
+	while (ht_curr_node)
+	{
+		if (strcmp(ht_curr_node->key, key) == 0)
+		{
+			free(ht_curr_node->value);
+			strcpy(ht_curr_node->value, value);
+			return (1);
+		}
+		ht_curr_node = ht_curr_node->next;
+	}
+
+	ht_curr_node = malloc(sizeof(hash_node_t));
+	if (!ht_curr_node)
+		return (0);
+
+	strcpy(ht_curr_node->key, key);
+	strcpy(ht_curr_node->value, value);
+	ht_curr_node->next = (ht->array)[index];
+	(ht->array)[index] = ht_curr_node;
+	return (1);
+}
