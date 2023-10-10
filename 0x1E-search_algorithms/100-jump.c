cd5c731e953bdcb2e42d25ffb9e03dc6db8079b5
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-10 13:03:18 -0700

    testing jumpsearch

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -7,6 +7,8 @@
  * @array: pointer to the first element of the array to search in
  * @size: number of element in array
  * @value: the value to search for
+ * Return: the first index where value is located,
+ * If value is not present in array or if array is NULL return -1
  */
 int jump_search(int *array, size_t size, int value)
 {
