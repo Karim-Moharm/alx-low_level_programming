6980ccd798351c4c98cf2617496a57c9e9f859b7
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-30 16:22:26 -0700

    fixing betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _memset - a function that fills memory with a constant byte
+ * @s: Starting address of memory to be filled
+ * @b: Value to be filled
+ * @n: Number of bytes to be filled
+ *
+ * Return: a pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
@@ -8,5 +16,5 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		*(s + i) = b;
 	}
-	return s;
+	return (s);
 }
